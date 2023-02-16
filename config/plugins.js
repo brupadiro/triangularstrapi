21b8158eb05534ebc9aec0551fd2bf1282c213f8
brupadiro <brunodiharce@gmail.com>
2023-02-16 12:48:33 -0300

    agregando socket y pagos

--- config/plugins.js
+++ config/plugins.js
@@ -28,6 +28,24 @@ module.exports = ({ env }) => ({
       }
     },
   }, 
-  
-  // ...
+  io: { 
+    "enabled": true,
+    "config": {
+      "IOServerOptions" :{
+        "cors": { "origin": "*", "methods": ["GET"] },
+      },
+      "contentTypes": {
+        "message": "*",
+        "chat":["create"]
+      },
+      "events":[
+        {
+          "name": "connection",
+          "handler": ({ strapi }, socket) => {
+            strapi.log.info(`[io] new connection with id ${socket.id}`);
+          },
+        },
+      ]
+    },
+  },
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,16 @@
+/*
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 11000),
   url: 'https://services.triangular.media',
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
+*/
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
